14984197956b90d69d8a5fd7dabe41860f369bcd
Enes <alesevic.enes1@gmail.com>
2021-09-18 17:45:24 +0200

    Commented out not working functionalities

--- cypress/integration/Automation Exercise/Exercise2.spec.js
+++ cypress/integration/Automation Exercise/Exercise2.spec.js
@@ -5,16 +5,41 @@ context("Exercise 2", () => {
 
         cy.get("@users").should((response) => {
             let data = response.body.data
+
             for (var i = 0; i < data.length; i++) {
                 expect(data[i].id).to.not.be.null
                 expect(data[i].email).contains(data[i].first_name.toLowerCase() + "." + data[i].last_name.toLowerCase() + "@reqres.in")
                 expect(data[i].avatar.split(".")[2]).to.equal("jpg")
             }
+
         })
     })
 
+    // it("Third Delete request", () => {
+    //     let deleteId = Math.floor(Math.random() * 6) + 1
+    //     cy.request("https://reqres.in/api/users/" + deleteId).as("deleteUser")
+
+    //     cy.get("@deleteUser").should((response) => {
+    //         let user = response.body.data
+    //         console.log(user[deleteId - 1])
+
+    //         cy.request('https://reqres.in/api/users').should((res) => {
+    //             let usersData = res.body.data
+    //             for (var i = 0; i < usersData.length; i++) {
+    //                 if (usersData[i].id == deleteId) {
+    //                     expect(user.email).to.equal(usersData[i].email)
+    //                     expect(user.first_name).to.equal(usersData[i].first_name)
+    //                     expect(user.last_name).to.equal(usersData[i].last_name)
+    //                 }
+    //             }
+    //         })
+
+    //     })
+    // })
+
     it("Fourth Get request", () => {
-        cy.request('https://reqres.in/api/users?delay=5').as("usersDelay")
+        let delayTime = Math.floor(Math.random() * 10) + 1
+        cy.request('https://reqres.in/api/users?delay=' + delayTime).as("usersDelay")
 
         cy.get("@usersDelay").should((response) => {
             let data = response.body.data
